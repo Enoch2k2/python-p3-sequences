b0d636a09d64c293ae94a24f51bb11ffd9e14704
Enoch2k2 <enoch2k2@hotmail.com>
2023-04-24 14:11:14 -0500

    finished

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list_items = []
+    for n in range(length):
+        if n < 2:
+            list_items.append(n)
+        else:
+            list_items.append(list_items[n-2] + list_items[n-1])
+    print(list_items)
